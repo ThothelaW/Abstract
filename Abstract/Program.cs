8941f5a2a2ad803b82a2465a079a9ed092e0e575
wthothela <wthothela@WKS-JHBE4-202>
2024-05-17 15:16:36 +0200

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Cat objCat = new Cat();
+        objCat.Shout();
+
+        Dog objDog = new Dog();
+        objDog.Shout();
+    }
+}
+
+
+public abstract class Animal
+{
+    public abstract string Sound { get; set; }
+
+    public abstract void Shout();
+}
+
+public class Cat : Animal
+{
+    public override string Sound
+    {
+        get { return "Meow"; }
+        set { }
+    }
+
+    public override void Shout()
+    {
+        Console.WriteLine($"Cat Sound {Sound}");
+    }
+}
+
+public class Dog : Animal
+{
+    public override string Sound
+    {
+        get { return "Woof"; }
+        set { }
+    }
+
+    public override void Shout()
+    {
+        Console.WriteLine($"Dog Sound {Sound}");
+    }
+}
